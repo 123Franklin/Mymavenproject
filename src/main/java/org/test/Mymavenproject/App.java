0424418a376a720d1daab964e68ea67f7ee4d049
123Franklin <franklin97.r@gmail.com>
2024-11-13 21:15:31 +0530

    created project

---
+++ src/main/java/org/test/Mymavenproject/App.java
@@ -0,0 +1,13 @@
+package org.test.Mymavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
