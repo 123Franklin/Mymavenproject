167974f557e8d556a7d476b30c4a9b2ba816d64c
123Franklin <franklin97.r@gmail.com>
2024-11-15 22:58:35 +0530

    created

--- src/main/java/org/test/Mymavenproject/App.java
+++ src/main/java/org/test/Mymavenproject/App.java
@@ -6,6 +6,13 @@ package org.test.Mymavenproject;
  */
 public class App 
 {
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
